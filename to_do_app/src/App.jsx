b16c1ceb842ae17aa242a148ed4259a5108ecfd2
KARAN-SHARXA <karansharma2302578@gmail.com>
2025-10-04 21:38:47 +0530

    to_do_app

---
+++ to_do_app/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <center class="todo-conatiner">
+      <h1>TODO APP</h1>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Todo Here"/>
+          </div>
+          <div class="col-4">col-4</div>
+          <div class="col-2">col-4</div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
